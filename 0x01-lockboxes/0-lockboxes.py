cf34d276149fc894482231c7346e6e1799445aff
KateM27 <mamocatherine@gmail.com>
2023-03-30 22:01:48 +0300

    correct output

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -15,6 +15,13 @@ def canUnlockAll(boxes):
                 break
         if unlocked is False:
             return unlocked
-        return True
+    return True
+
 # boxes = [[1], [2], [3], [4], []]
+# print(canUnlockAll(boxes))
+
+# boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+# print(canUnlockAll(boxes))
+
+# boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 # print(canUnlockAll(boxes))
