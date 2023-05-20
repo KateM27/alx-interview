adde1cd38d5d81cd2f290f2f91d95fd6bbb4920f
KateM27 <mamocatherine@gmail.com>
2023-05-20 10:10:54 +0300

    update readme

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,23 +4,27 @@
 
 
 def makeChange(coins, total):
-    """This function will take a list of coins and use
-       that to calculate how much change the total will require
-    """
+    """Adding two arrays"""
     if total <= 0:
         return 0
 
-    else:
-        coin = sorted(coins)
-        coin.reverse()
-        counter = 0
-        for i in coin:
-            while(total >= i):
-                counter += 1
-                total -= i
-        if total == 0:
-            return counter
+    # verify coins is a valid
+    if (coins is None or len(coins) == 0):
         return -1
+
+    change = 0
+    my_coins = sorted(coins, reverse=True)
+    money_left = total
+
+    for coin in my_coins:
+        while (money_left % coin >= 0 and money_left >= coin):
+            change += int(money_left / coin)
+            money_left = money_left % coin
+
+    change = change if money_left == 0 else -1
+
+    return change
+
 
 print(makeChange([1, 2, 25], 37))
 print(makeChange([1256, 54, 48, 16, 102], 1453))
